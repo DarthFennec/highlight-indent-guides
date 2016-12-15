a1eeb2e50ca51b5b2e3a5cd190d2f1d79cf6ad28
travisf <travisf@jfrog.com>
2016-12-14 16:19:48 -0800

    Fixed invisible indents bug
    Inserting indentation manually (by pressing the spacebar) could
    sometimes lead to the inserted spaces appearing invisible. This is due
    to a combination of three behaviors:
    
    1. When a character is typed, it inherits the properties from the
       preceding character, by default.
    2. For performance reasons, this mode does not re-highlight lines that
       already appear to be highlighted correctly.
    3. When a series of characters all have `eq' display properties, they
       are replaced with the display string as a group.
    
    When a space is inserted after a space that is displayed as an indent
    rule character, the properties are inherited, including the display
    property. These display properties are not overwritten when the line is
    still deemed "correct". This causes all such spaces to be combined, and
    displayed as a single indent rule.
    
    This is now fixed: the line reader now considers it invalid to have
    either a starter character following a character of the same color, or a
    non-starter character following a character of a different color
    (before, it didn't consider the starter property at all). This causes
    the offending lines to be redrawn.

--- highlight-indent-guides.el
+++ highlight-indent-guides.el
@@ -21,7 +21,7 @@
 ;; SOFTWARE.
 ;;
 ;; Author: DarthFennec <darthfennec@derpymail.org>
-;; Version: 0.6.5
+;; Version: 0.6.6
 ;; Package-Requires: ((emacs "24"))
 ;; URL: https://github.com/DarthFennec/highlight-indent-guides
 
@@ -103,11 +103,14 @@ and INDENT is this line's indent width."
 (defun highlight-indent-guides--get-guides ()
   "Extract the indent guides from a line, by reading the text properties."
   (save-excursion
-    (let (prop face seg nface nseg invalid guides)
+    (let (prop face seg nface nseg invalid guides fst)
       (while (and (not invalid) (looking-at "[[:space:]]"))
         (setq prop (get-text-property (point) 'highlight-indent-guides-prop))
-        (setq nface (car prop) nseg (nth 1 prop))
+        (setq nface (car prop) nseg (nth 1 prop) fst (nth 2 prop))
         (unless (or (eq nface 'odd) (eq nface 'even)) (setq invalid t))
+        (unless (or invalid seg nseg)
+          (when (and fst (eq face nface)) (setq invalid t))
+          (when (not (or fst (eq face nface))) (setq invalid t)))
         (unless (or invalid (and (equal face nface) (equal seg nseg)))
           (when (and face (not (equal face nface)))
             (setq guides (cons (current-column) guides)))
